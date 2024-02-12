5f73a2a02d995abb0e9357769d0f2c1d92ccbecb
Kajanan <Kajanan15i@gmail.com>
2024-02-12 15:15:26 +0000

    Created a test class for the Car class and added test cases for methods prior to their  implementation

---
+++ src/test/java/CarTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CarTest {
+
+    Car car;
+
+    @BeforeEach
+    public void setUp(){
+        car = new Car(4, "Petrol", "Combustion", 75000, 520, true, 4, true, true);
+    }
+
+    @Test
+    public void canAccelerate(){
+        assertThat(car.accelerate()).isEqualTo("Vroom");
+    }
+
+    @Test
+    public void canBreak(){
+        assertThat(car.breaking()).isEqualTo("Squeek... Stopped!");
+    }
+
+    @Test
+    public void canPlayMusic(){
+        assertThat(car.playMusic()).isEqualTo("Travis Scott");
+    }
+
+
+
+}
