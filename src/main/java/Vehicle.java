914580c4718448f0acd5c82cce5c2ca95a47f125
Kajanan <Kajanan15i@gmail.com>
2024-02-12 15:02:35 +0000

    Created the Vehicle class and added its attributes and methods

---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,28 @@
+public abstract class Vehicle {
+    private int maxCapacity;
+    private String fuelType;
+    private String motorType;
+    private int price;
+    private int maxSpeedBHP;
+
+    public Vehicle(int maxCapacity, String fuelType, String motorType, int price, int maxSpeedBHP){
+        this.maxCapacity = maxCapacity;
+        this.fuelType = fuelType;
+        this.motorType = motorType;
+        this.price = price;
+        this.maxSpeedBHP = maxSpeedBHP;
+    }
+
+    public String accelerate(){
+        return "Vroom";
+    }
+
+    public String breaking(){
+        return "Squeek... Stopped!";
+    }
+
+    public String honk(){
+        return "Toot toot!";
+    }
+
+}
